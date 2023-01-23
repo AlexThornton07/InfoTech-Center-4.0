9d7fe3dcce007e45ac7b2877fd141bf6d427e64a
Alex Thornton <alexander.thornton07@gmail.com>
2023-01-23 13:49:44 -0500

    Update Welcome Message

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,3 +1,11 @@
 # Programmer: Alex Thornton
 # Date: 1.20.23
-# Program: Infotech Center Uprgrades
+# Program: InfoTech Center Upgrades
+
+"""
+Our Welcome Screen will start our Program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+print("\n\nWelcome - InfoTechCenter 4.0 ")
+print("\nInfoTech Center 4.0 OS is loading")
