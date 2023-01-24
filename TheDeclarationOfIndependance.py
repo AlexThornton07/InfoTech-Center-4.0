7db6492e16675f4b2be4fffe8f3564722219954c
Alex Thornton <alexander.thornton07@gmail.com>
2023-01-24 14:07:25 -0500

    Add ... to my loading page

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -9,10 +9,28 @@ drivers know that the InfoTech Center 4.0 OS is loading
 
 # Import Libraries Hear
 import time
-
-timeToSleep = 2
+import sys
 
 print("\n\nWelcome - InfoTechCenter 4.0 ")
-time.sleep(timeToSleep)
-print("\nInfoTech Center 4.0 OS is loading")
+
+x = 0
+a = 0
+
+timeToSleep = 2
+print('')
+
+while x != 20:
+    x += 1
+    b = ("\033[1;44;40m InfoTechCenter 4.0 OS is Loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b)
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[1;32;40m Done!')
+
+
+
+
 
