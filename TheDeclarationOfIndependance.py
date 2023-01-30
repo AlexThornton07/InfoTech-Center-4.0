bac388e8b6171f37557ead5bf61ac509489e69a7
Alex Thornton <alexander.thornton07@gmail.com>
2023-01-30 13:28:12 -0500

    Update...on Print Statement - Added Color - Changed Text & Spaces

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -11,7 +11,7 @@ drivers know that the InfoTech Center 4.0 OS is loading
 import time
 import sys
 
-print("\n\nWelcome - InfoTechCenter 4.0 ")
+print("\n\n\033[1;44;40mWelcome - InfoTechCenter 4.0 ")
 
 x = 0
 a = 0
@@ -21,14 +21,14 @@ print('')
 
 while x != 20:
     x += 1
-    b = ("\033[1;44;40m InfoTechCenter 4.0 OS is Loading" + "." * a)
+    b = ("\033[1;44;40mInfoTechCenter 4.0 OS is Loading" + "." * a)
     a = a + 1
     sys.stdout.write('\r'+b)
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[1;32;40m Done!')
+        print('\033[1;32;40mMission Accomplished - Retina Scanned - Access Granted')
 
 
 
