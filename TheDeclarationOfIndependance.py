3600803c199e355a5156d394fe832a1ddbf1713f
Alex Thornton <alexander.thornton07@gmail.com>
2023-01-30 14:19:14 -0500

    gasLevelGauge Function Declared and Stable

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,3 +1,30 @@
 # Programmer: Alex Thornton
-# Date: 1.20.23
-# Program: Infotech Center Uprgrades
+# Date: 1.30.23
+# Program: InfoTech Center 4.0 - Gasoline
+
+"""
+We will create a Function that will tell us our Fuel Gauge Level
+  -Create a List with Gas Levels
+  -Randomize and choose from the list to determine our gas level
+
+Create a Function to determine our closest Gas Station
+  -Create a List of gas stations
+  -Randomly choose a gas station from the list
+
+Create a Function to determine our gas level and closest gas station
+  -Print Gas Level
+  -Print Gas Station
+"""
+
+# Import Libraries Here
+import random
+
+
+# Gas Level Function
+def gasLevelGauge():
+    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
+
+print(gasLevelGauge())
+
