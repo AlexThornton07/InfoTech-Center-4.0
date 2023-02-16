00a81f55adeea2d833e7c87ada425cf94e2195bf
Alex Thornton <alexander.thornton07@gmail.com>
2023-02-16 14:14:38 -0500

    2-16-23 Unstable

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,3 +1,45 @@
 # Programmer: Alex Thornton
 # Date: 1.20.23
 # Program: Infotech Center Uprgrades
+
+"""
+I will be creating a system that adjusts to the drivers preferences 
+"""
+
+#Import Libraries Here:
+random
+
+def Drivers():
+    listOfDrivers = ["Driver 1","Driver 2","Driver 3","Driver 4"]
+    chooseDriver = random.choice(listOfDrivers)
+    return driver
+
+driverAlert = Drivers()
+
+def Radio():
+    listOfRadioStations = ["Rock","Pop","Hip Hop","Country","Top Hits"]
+    chooseRadioStation = random.choice(listOfRadioStations)
+
+def SteeringWheel():
+    listOfSteeringWheelPostions = ["Low","Medium","High"]
+    chooseSteeringWheelPostion = random.choice(listOfSteeringWheelPostions)
+
+def Seat():
+    listOfSeatSettings = ["Close","Pop","Middle","Far"]
+    chooseSeatSetting = random.choice(listOfSeatSettings)
+
+#Show Driver Settings
+
+def driverSettings():
+    if "Driver 1":
+        print("Driver 1 has been chosen")
+        print("The radio has been cha")
+    elif "Driver 2":
+        print("Driver 2 has been chosen")
+    elif "Driver 3":
+        print("Driver 3 has been chosen")
+    else:
+        print("Driver 4 has been chosen")
+
+driverSettings():
+
